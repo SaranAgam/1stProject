63f290fee40e1104b0c826ec87a6ab2d1f3be692
Agam Saran <agamsaran.as@gmail.com>
2025-09-10 22:27:03 +0530

    updated

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -6,10 +6,9 @@ namespace _1stProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("This is a C# 7.3 project targeting .NET Framework 4.7.2");
-            Console.WriteLine("Enjoy coding!");
-            Console.ReadLine();
+            Console.WriteLine("Write your city name: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("You are from: " (city));
         }
 
     }
