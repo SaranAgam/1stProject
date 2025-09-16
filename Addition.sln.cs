d16448198f669f86f86a4e26004b71fb6cd0a75f
Agam Saran <agamsaran.as@gmail.com>
2025-09-16 23:31:25 +0530

    Add files via upload
    new file

---
+++ Addition.sln.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		Console.WriteLine("Enter the numbers: ");
+		int num1 = Convert.ToInt32(Console.ReadLine());
+		int num2 = Convert.ToInt32(Console.ReadLine());
+		int sum = num1 + num2;
+		Console.WriteLine("The sum of two numbers is: " + sum);
+    }
+}
