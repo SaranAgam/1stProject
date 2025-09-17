45fed950f830f4abc4288ec3d88d4a40c7cf2b40
Agam Saran <agamsaran.as@gmail.com>
2025-09-17 23:15:41 +0530

    update

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -6,7 +6,7 @@ namespace _1stProject
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter any key from keyboard : ");
+            Console.Write("Enter any 2 numbers from keyboard : ");
             int key = Console.Read();
             Console.WriteLine("The ascii key value is: " + key);
         }
