5ee86533f735836ab9d5c164023aab345e32df19
Agam Saran <agamsaran.as@gmail.com>
2025-09-17 23:25:39 +0530

    updated

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -7,7 +7,9 @@ namespace _1stProject
         static void Main(string[] args)
         {
             Console.Write("Enter any 2 numbers from keyboard : ");
-            int key = Console.Read();
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int key = num1 + num2;
             Console.WriteLine("The ascii key value is: " + key);
         }
 
