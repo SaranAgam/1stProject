f757bd0b61f66125f510bcad6c972218c10cf6aa
Agam Saran <agamsaran.as@gmail.com>
2025-09-24 19:26:17 +0530

    up

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -9,7 +9,7 @@ namespace _1stProject
             Console.WriteLine("Enter any 2 numbers from keyboard: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
             double num2 = Convert.ToDouble(Console.ReadLine());
-            double sum = num1 + num2;
+            double sum = num1+num2;
             double difference = num1-num2;
             double product = num1 * num2;
             double quotient = num1 / num2;
