f147f90b870795eced413676d5ee0ced4a6a3b3d
Agam Saran <agamsaran.as@gmail.com>
2025-09-24 19:26:36 +0530

    up

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -11,8 +11,8 @@ namespace _1stProject
             double num2 = Convert.ToDouble(Console.ReadLine());
             double sum = num1+num2;
             double difference = num1-num2;
-            double product = num1 * num2;
-            double quotient = num1 / num2;
+            double product = num1*num2;
+            double quotient = num1/num2;
             Console.WriteLine("The sum of given 2 numbers is: " + sum);
             Console.WriteLine("The difference of given 2 numbers is: " + difference);
             Console.WriteLine("The product of given 2 numbers is: " + product);
