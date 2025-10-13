34386bf150aaddbfc5ed9cdf5e080edfa5521b8d
Agam Saran <agamsaran.as@gmail.com>
2025-10-13 22:17:05 +0530

    up

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -15,7 +15,7 @@ namespace _1stProject
             double quotient = num1/num2;
             Console.WriteLine("Sum of: " + sum);
             Console.WriteLine("Difference of: " + difference);
-            Console.WriteLine("The Product of: " + product);
+            Console.WriteLine("Product of: " + product);
             Console.WriteLine("The Quotient of: " + quotient);
         }
 
