41e53eaffdd73cef088264ce5974b2248eefca18
Agam Saran <agamsaran.as@gmail.com>
2025-10-28 22:31:38 +0530

    up

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -12,11 +12,9 @@ namespace _1stProject
             double sum = num1+num2;
             double difference = num1-num2;
             double product = num1*num2;
-            double quotient 
             Console.WriteLine("Sum of the given: " + sum);
             Console.WriteLine("Difference of the given: " + difference);
             Console.WriteLine("Product of the: " + product);
-            Console.WriteLine("Quotient of the: " + quotient);
         }
 
     }
