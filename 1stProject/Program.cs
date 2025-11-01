0853975f5da95432b2787308233fd6958e6d84ad
Agam Saran <agamsaran.as@gmail.com>
2025-11-01 22:04:27 +0530

    up

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -12,7 +12,7 @@ namespace _1stProject
             double sum = num1+num2;
             double difference = num1-num2;
             Console.WriteLine("Sum of the given: " + sum);
-            Console.WriteLine("Difference of the
+            Console.WriteLine(
          
         }
 
